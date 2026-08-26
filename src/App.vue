bdcf55788933eaefe0fe17b1d049bf24418955d4
SUHO PARK <duglikespiano@gmail.com>
2026-08-26 21:13:23 +0900

    FEATURE - VUE COMPONENT ADDED

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,51 @@
-<script setup></script>
+<script setup>
+/*
+	const { createApp, ref } = Vue;
+	createApp({
+	  setup() {
+	    const name = ref('Dug');
+	    const emoji = ref('✌🏻');
+	    return { name, emoji };
+	  }
+	}).mount('#app');
+	*/
+import { ref } from 'vue';
+const name = ref('Dug');
+const emoji = ref('✌🏻');
+const year = ref('2014');
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+	<header>
+		<span>{{ emoji }}</span>
+	</header>
+
+	<main>
+		<h1>Hello, {{ name }}!</h1>
+		<p>I'm about to learn <a href="https://vuejs.org/" target="_blank">Vue.js</a>!</p>
+	</main>
+
+	<footer>
+		<p>&copy; {{ year }}</p>
+	</footer>
 </template>
 
-<style scoped></style>
+<style scoped>
+header span {
+	font-size: 3rem;
+}
+main h1 {
+	display: block;
+}
+main a {
+	color: #42b883;
+}
+footer {
+	position: fixed;
+	bottom: 0;
+	padding-bottom: 20px;
+}
+footer p {
+	color: blue;
+}
+</style>
