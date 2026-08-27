7f3a80c4d7f0d54f01b54f7882761d244a3cc4d9
SUHO PARK <duglikespiano@gmail.com>
2026-08-27 21:06:43 +0900

    FEATURE - LAYOUT COMPONENTS ADDED

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,12 @@
 <script setup>
-/*
-	const { createApp, ref } = Vue;
-	createApp({
-	  setup() {
-	    const name = ref('Dug');
-	    const emoji = ref('✌🏻');
-	    return { name, emoji };
-	  }
-	}).mount('#app');
-	*/
-import { ref } from 'vue';
-const name = ref('Dug');
-const emoji = ref('✌🏻');
-const year = ref('2014');
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
-	<header>
-		<span>{{ emoji }}</span>
-	</header>
-
-	<main>
-		<h1>Hello, {{ name }}!</h1>
-		<p>I'm about to learn <a href="https://vuejs.org/" target="_blank">Vue.js</a>!</p>
-	</main>
-
-	<footer>
-		<p>&copy; {{ year }}</p>
-	</footer>
+	<Header />
+	<Main />
+	<Footer />
 </template>
-
-<style scoped>
-header span {
-	font-size: 3rem;
-}
-main h1 {
-	display: block;
-}
-main a {
-	color: #42b883;
-}
-footer {
-	position: fixed;
-	bottom: 0;
-	padding-bottom: 20px;
-}
-footer p {
-	color: blue;
-}
-</style>
+<style scoped></style>
